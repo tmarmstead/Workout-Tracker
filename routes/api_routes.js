eb56bdc03cb95014da5c694ef37449d50342bf5f
Tina Armstead <tina.armstead@gmail.com>
2021-03-18 21:35:17 -0400

    Added api route outline

--- routes/api_routes.js
+++ routes/api_routes.js
@@ -1,2 +1,18 @@
 const router = require('express').Router();
 const Workout = require('../models/workout.js');
+
+router.get('api/workouts', (req, res) => {
+
+})
+
+router.get('api/workouts/range', (req, res) => {
+    
+})
+
+router.post('api/workouts', (req, res) => {
+
+})
+
+router.put('api/workouts/:id', (req, res) => {
+
+})
